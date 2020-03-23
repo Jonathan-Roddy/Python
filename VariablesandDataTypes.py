0166ef5e53dbc07f175d091e751c0d974ca6d0fc
Jonathan Roddy <49447734+Jonathan-Roddy@users.noreply.github.com>
2020-03-23 16:08:56 +0000

    Create VariablesandDataTypes.py

---
+++ VariablesandDataTypes.py
@@ -0,0 +1,20 @@
+# Variables     one line comment
+'''  multiline comment
+Names are case-sensitive and may begin with:
+   letters, $, _
+After, may include
+   letters, numbers, $, _
+Convention says
+   Start with a lowercase word, then additional words are separated
+   by underscores
+   ex. my_first_variable
+'''
+name = "Jonathan"    # Strings
+age = 24         # Integer
+gpa = 3.5         # Decimal
+is_tall = True    # Boolean -> True/False
+
+name = "Roddy"
+
+print("Your name is " + name)
+print("Your name is", name)
